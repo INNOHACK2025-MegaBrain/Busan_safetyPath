26b1630c233cbb224dd52c0f90c44c143538d03b
KJWGarden <mike03131313@gmail.com>
2025-11-28 02:58:57 +0900

    style: 지도 풀화면으로 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import BasicMap from "@/components/KakaoMap";
 
 export default async function Home() {
   return (
-    <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="w-full h-screen">
       <main className="w-full h-full">
         <BasicMap />
       </main>
