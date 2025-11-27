b2d628420c103a4b9f063df93bab177ac3baf1e0
KJWGarden <mike03131313@gmail.com>
2025-11-28 01:47:43 +0900

    feat: 지도 현재위치 상에 마커 표시
    지도 현재위치 받아온 뒤, 현재 위치에 마커 표시 추가

--- components/KakaoMap.tsx
+++ components/KakaoMap.tsx
@@ -2,7 +2,9 @@
 
 import { Map } from "react-kakao-maps-sdk";
 import useKakaoLoader from "@/hooks/useKakaoLoader";
-import { useState, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
+import AddMapCustomControlStyle from "./addMapCustomControl.style";
+import { MapMarker } from "react-kakao-maps-sdk";
 
 interface Position {
   lat: number;
@@ -11,6 +13,9 @@ interface Position {
 
 export default function BasicMap() {
   useKakaoLoader();
+  const mapRef = useRef<kakao.maps.Map>(null);
+  const [mapType, setMapType] = useState<"roadmap" | "skyview">("roadmap");
+
   const [position, setPosition] = useState<Position>({
     lat: 33.450701, // 기본값 (제주도)
     lng: 126.570667,
@@ -36,7 +41,9 @@ export default function BasicMap() {
       );
     } else {
       console.error("Geolocation을 지원하지 않는 브라우저입니다.");
-      setLoading(false);
+      queueMicrotask(() => {
+        setLoading(false);
+      });
     }
   }, []);
 
@@ -49,17 +56,46 @@ export default function BasicMap() {
   }
 
   return (
-    <Map
-      id="map"
-      center={{
-        lat: position.lat,
-        lng: position.lng,
-      }}
-      style={{
-        width: "100%",
-        height: "350px",
-      }}
-      level={3}
-    />
+    <>
+      <AddMapCustomControlStyle />
+      <div className={`map_wrap`}>
+        <Map
+          id="map"
+          center={{
+            lat: position.lat,
+            lng: position.lng,
+          }}
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "relative",
+            overflow: "hidden",
+          }}
+          level={3}
+          mapTypeId={mapType === "roadmap" ? "ROADMAP" : "HYBRID"}
+          ref={mapRef}
+        >
+          {!!position && <MapMarker position={position} title="현재 위치" />}
+        </Map>
+        <div className="custom_typecontrol radius_border">
+          <span
+            id="btnRoadmap"
+            className={mapType === "roadmap" ? "selected_btn" : "btn"}
+            onClick={() => setMapType("roadmap")}
+          >
+            지도
+          </span>
+          <span
+            id="btnSkyview"
+            className={mapType === "skyview" ? "selected_btn" : "btn"}
+            onClick={() => {
+              setMapType("skyview");
+            }}
+          >
+            스카이뷰
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
