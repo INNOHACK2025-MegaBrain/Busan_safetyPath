50f35745138be3017d80868dfa526797152422ba
SeoSangHyeok <ssb50800@gmail.com>
2025-11-29 05:32:32 +0900

    fix: 빌드 오류 수정

--- components/common/KakaoMap.tsx
+++ components/common/KakaoMap.tsx
@@ -3,6 +3,7 @@
 import { Map, Polyline, CustomOverlayMap } from "react-kakao-maps-sdk";
 import useKakaoLoader from "@/hooks/useKakaoLoader";
 import { useRef, useState, useEffect, useMemo, useCallback } from "react";
+import type { ReactElement } from "react";
 import AddMapCustomControlStyle from "./addMapCustomControl.style";
 import { MapMarker } from "react-kakao-maps-sdk";
 import {
@@ -783,7 +784,7 @@ export default function BasicMap() {
     const sosIcon =
       "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%220%200%2040%2040%22%3E%3Ccircle%20cx%3D%2220%22%20cy%3D%2220%22%20r%3D%2218%22%20fill%3D%22white%22%20stroke%3D%22%23DC2626%22%20stroke-width%3D%223%22%2F%3E%3Ctext%20x%3D%2220%22%20y%3D%2225%22%20text-anchor%3D%22middle%22%20font-size%3D%2212%22%20font-family%3D%22Arial%22%20font-weight%3D%22bold%22%20fill%3D%22%23DC2626%22%3ESOS%3C%2Ftext%3E%3C%2Fsvg%3E";
 
-    const elements: JSX.Element[] = [];
+    const elements = [] as ReactElement[];
 
     sosTargets
       .filter((target) => !target.triggeredByMe)
